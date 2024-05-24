166c47d0eac2ec5e580ceaa12ec0ed5842dc1646
Mike ONeil <MoProductions01@gmail.com>
2024-05-23 23:27:36 -0600

    Another big change coming up so create a save state.

--- Assets/Scripts/Dax/PuzzleObjects/BoardObject.cs
+++ Assets/Scripts/Dax/PuzzleObjects/BoardObject.cs
@@ -3,28 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+/// <summary>
+/// BoardObject is the base class for all the objects on the board
+/// </summary>
 public class BoardObject : MonoBehaviour
 {    
+    // All the types of BoardObjects that we can have
     public enum eBoardObjectType { PLAYER, FACET, HAZARD, FACET_COLLECT, SHIELD, SPEED_MOD, POINT_MOD }; 
     public eBoardObjectType BoardObjectType;
 
-    public enum eStartDir {OUTWARD, INWARD}; // monewsave
+    // Strings used in the DaxEditor to show in the Inspector window
+    public static List<string> BOARD_OBJECT_EDITOR_NAMES = new List<string> {"Player", "Facet", "Hazard", "Facet Collect", 
+                                                                                "Shield", "Speed Mod", "Point Mod"};  
+
+    // Movement info.  
+    public enum eStartDir {OUTWARD, INWARD}; 
     public eStartDir StartDir = eStartDir.OUTWARD;
+    public float Speed = 0f;    
 
     // Shared      
     [HideInInspector]
-    public Dax _Dax;                  
-    public Channel CurChannel = null;
-       
-    public float Speed = 0f;    
-
+    public Dax _Dax;   // Root Dax game object ref               
+    public Channel CurChannel = null;   // Current channel that the board object is in
+           
     [Header("Starting State Stuff")]
-    public ChannelNode SpawningNode = null;    
+    public ChannelNode SpawningNode = null;    // The node you spawn on 
 
+    /// <summary>
+    /// Handles generic/universal initialization for BoardObjects
+    /// </summary>
+    /// <param name="spawnNode">Node we're spawning on</param>
+    /// <param name="dax">Root game reference</param>
     public virtual void InitForChannelNode(ChannelNode spawnNode, Dax dax)
     {                
         _Dax = dax;              
-        if (spawnNode != null) // if it's not the player do the spawn stuff right away moupdate - we now have another thing for initting on player
+        if (spawnNode != null)
         {
             SpawningNode = spawnNode;
             spawnNode.SpawnedBoardObject = this;
@@ -34,36 +47,34 @@ public class BoardObject : MonoBehaviour
             transform.LookAt(CurChannel.EndNode.transform);
         }        
     }   
-
-    public static List<string> BOARD_OBJECT_EDITOR_NAMES = new List<string> {"Player", "Facet", "Hazard", "Facet Collect", 
-                                                                                "Shield", "Speed Mod", "Point Mod"};   
-
-    
+         
+    /// <summary>
+    /// Checks for the transition from one channel to another for any moving game objects
+    /// </summary>
+    /// <param name="overlapColliders">List of colliders we're currently overlapping</param>
     private void CheckForNewChannel(List<Collider> overlapColliders)
-    {
-        // Channel nodes
-        List<Collider> cnColliders = overlapColliders.ToList();
-        cnColliders.RemoveAll(c => c.GetComponent<ChannelNode>() == null);                        
-        cnColliders.RemoveAll(c => c.GetComponent<ChannelNode>().CanBeOnPath() == false);            
-        cnColliders.RemoveAll(c => c.GetComponent<ChannelNode>().MyChannel == CurChannel);   
-        cnColliders.RemoveAll(c => Vector3.Dot( (c.transform.position - this.transform.position), this.transform.forward) <= 0);                     
-        if(cnColliders.Count > 0)
+    {        
+        List<Collider> channelNodeColliders = overlapColliders.ToList(); // Make a new list of the colliders we're overlapping
+        channelNodeColliders.RemoveAll(c => c.GetComponent<ChannelNode>() == null); // Remove anything that's not a ChannelNode
+        channelNodeColliders.RemoveAll(c => c.GetComponent<ChannelNode>().CanBeOnPath() == false);   // Remove any paths we can't be on         
+        channelNodeColliders.RemoveAll(c => c.GetComponent<ChannelNode>().MyChannel == CurChannel);   // Remove our channel if necessary
+        channelNodeColliders.RemoveAll(c => Vector3.Dot( (c.transform.position - this.transform.position), this.transform.forward) <= 0); // Remove anything behind us                     
+        if(channelNodeColliders.Count > 0)
         {
-            if(cnColliders.Count > 1) 
+            if(channelNodeColliders.Count > 1) 
             {
-                Debug.LogError("Why do we have more than 1 Channel Node Colliders?: " + cnColliders.Count);                    
+                Debug.LogError("Why do we have more than 1 Channel Node Colliders?: " + channelNodeColliders.Count);                    
             }
 
-            ChannelNode cn = cnColliders[0].GetComponent<ChannelNode>();
-            Vector3 heading = cn.transform.position - this.transform.position;
+            ChannelNode channelNode = channelNodeColliders[0].GetComponent<ChannelNode>();
+            Vector3 heading = channelNode.transform.position - this.transform.position;
             float dot = Vector3.Dot(heading, this.transform.forward);         
-            if(dot > 0f) // monote - get rid of these checks since they're supposed to be handled above
-            {
-                CurChannel = cn.MyChannel;                
-                this.transform.parent = CurChannel.MyRing.transform;
-                //LastPositionObject.transform.parent = CurChannel.MyRing.transform;                
-                this.transform.position = cn.transform.position;
-                transform.LookAt(cn.IsStartNode() ? cn.MyChannel.EndNode.transform : cn.MyChannel.StartNode.transform);      
+            if(dot > 0f) // Make sure channel node is in front of us.  It should be but better safe than sorry
+            {   // If we're here then we can move to a new channel
+                CurChannel = channelNode.MyChannel;                
+                this.transform.parent = CurChannel.MyRing.transform;                
+                this.transform.position = channelNode.transform.position;
+                transform.LookAt(channelNode.IsStartNode() ? channelNode.MyChannel.EndNode.transform : channelNode.MyChannel.StartNode.transform);      
             } 
             else
             {
@@ -72,25 +83,28 @@ public class BoardObject : MonoBehaviour
         }      
     }
 
-    private void CheckGenericBounce(List<Collider> pgColliders)
+    /// <summary>
+    /// Check for a moving BoardObject colliding with something and bouncing back in the other direction
+    /// </summary>
+    /// <param name="overlapColliders">List of colliders we're overlapping with.</param>
+    private void CheckGenericBounce(List<Collider> overlapColliders)
     {
-        // Player Generic (walls, etc)
-        //List<Collider> pgColliders = overlapColliders.ToList();
-        pgColliders.RemoveAll(c => c.gameObject.layer != LayerMask.NameToLayer("Player Generic Collider"));  
-        pgColliders.RemoveAll(c => Vector3.Dot( (c.transform.position - this.transform.position), this.transform.forward) <= 0);            
-        if(pgColliders.Count > 0)
+        // Player Generic (walls, etc)        
+        overlapColliders.RemoveAll(c => c.gameObject.layer != LayerMask.NameToLayer("Player Generic Collider"));  
+        // Remove anything behind us
+        overlapColliders.RemoveAll(c => Vector3.Dot( (c.transform.position - this.transform.position), this.transform.forward) <= 0);            
+        if(overlapColliders.Count > 0)
         {               
-
-            if(pgColliders.Count > 1) 
-            {                
-                pgColliders = pgColliders.OrderBy(o => Vector3.Distance(o.transform.position, this.transform.position)).ToList();                   
+            if(overlapColliders.Count > 1) 
+            {   // Sort overlapping colliders by distance     
+                overlapColliders = overlapColliders.OrderBy(o => Vector3.Distance(o.transform.position, this.transform.position)).ToList();                   
             }
 
-            Collider pg = pgColliders[0];
-            Vector3 heading = pg.transform.position - this.transform.position;
+            Collider collider = overlapColliders[0];
+            Vector3 heading = collider.transform.position - this.transform.position;
             float dot = Vector3.Dot(heading, this.transform.forward);         
             if(dot > 0f)
-            {                         
+            {   // If the colliders is in front of us then bounce back in the other direction                      
                 transform.forward = -transform.forward;         
             } 
             else
@@ -100,46 +114,50 @@ public class BoardObject : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Handles colliding with a bumper
+    /// </summary>
+    /// <param name="bumperColliders">Bumper colliders we're overlapping with</param>
     private void CheckBumpers(List<Collider> bumperColliders)
-    {
-        // BUMPER collisions                
-       // List<Collider> bumperColliders = curSphereColliders.ToList();
+    {        
+        // Quick error checking
         for(int i=0; i<bumperColliders.Count; i++)
         {
             if(bumperColliders[i] == null)
             {
-                Debug.LogError("wtF");
+                Debug.LogError("Null bumper collider");
+                return;
             }
         }
+        // Remove all colliders that aren't bumpers
         bumperColliders.RemoveAll(x => x.GetComponent<Bumper>() == null);
         if (bumperColliders.Count > 0)
         {
             Bumper bumper = bumperColliders[0].GetComponent<Bumper>();
             if (this.BoardObjectType == eBoardObjectType.PLAYER)
-            {
+            {   // Only Players have to check for things like matching facet colors.  Enemies just bounce regardless
                 Player player = FindObjectOfType<Player>();
                 switch (bumper.BumperType)
                 {
                     case Bumper.eBumperType.DEATH:
+                        // Collided with a Death bumper so game over
                         FindObjectOfType<Dax>().EndGame("Death Bumper!");
                         break;
                     case Bumper.eBumperType.COLOR_MATCH:
-                        if (player.CarriedColorFacet == null) break;
+                        // Color match collider so check it against any facets the player is carrying
+                        if (player.CarriedColorFacet == null) break; // No facet being carried so bail
                         Facet playerCarriedColorFacet = (Facet)player.CarriedColorFacet;
                         if (playerCarriedColorFacet._Color == bumper.BumperColor)
-                        {                                
-                            FindObjectOfType<Dax>().Wheel.MatchedFacetColor(playerCarriedColorFacet);
-                            //curSphereColliders.Remove(playerCarriedColorFacet.GetComponentInChildren<Collider>());
+                        {   // Player is carrying a facet that's the same color as the bumper so update game state                           
+                            FindObjectOfType<Dax>().Wheel.MatchedFacetColor(playerCarriedColorFacet);                            
                             DestroyImmediate(playerCarriedColorFacet.gameObject);
                             if(_Dax.Wheel.VictoryCondition == Dax.eVictoryConditions.COLOR_MATCH) _Dax.Wheel.CheckVictoryConditions();
                         }
                         break;
                 }
             }
-            transform.forward = -transform.forward;   
-           // transform.position = LastPositionObject.transform.position;
-            //MoveDir = (MoveDir == eMoveDir.INWARD ? eMoveDir.OUTWARD : eMoveDir.INWARD);
-            //Debug.Log("bounced off: " + bumperColliders[0].name);
+            // Every object bounces back to the other direction
+            transform.forward = -transform.forward;             
         }
     }
 
@@ -299,29 +317,7 @@ public class BoardObject : MonoBehaviour
                   //  case SpeedMod.eSpeedModType.RING_DOWN:
                         float speedModVal2 = (speedMod.SpeedModType == SpeedMod.eSpeedModType.RING_SPEED ? speedMod.SpeedModVal : -speedMod.SpeedModVal);
                         player.CurChannel.MyRing.RotateSpeed += speedModVal2;
-                        break;
-                 /*   case SpeedMod.eSpeedModType.WHEEL_UP:
-                    case SpeedMod.eSpeedModType.WHEEL_DOWN:
-                        float speedModVal3 = (speedMod.SpeedModType == SpeedMod.eSpeedModType.WHEEL_UP ? speedMod.SpeedModVal : -speedMod.SpeedModVal);
-                        List<Ring> rings = _Dax.CurWheel.GetComponentsInChildren<Ring>().ToList();
-                        foreach (Ring ring in rings) ring.RotateSpeed += speedModVal3; // moupdate - move this to the actual script so that it's not bloating up all this
-                        break;    */                            
-                   /* case SpeedMod.eSpeedModType.RING_STOP:
-                        player.CurChannel.MyRing.RotateSpeed = 0f;
-                        break;
-                    case SpeedMod.eSpeedModType.TIME_STOP: // moupdate - maybe split this up from SpeedMod to EnvMod or something
-                        List<Ring> rings2 = _Dax.CurWheel.GetComponentsInChildren<Ring>().ToList();
-                        List<Hazard> enemies2 = _Dax.CurWheel.GetComponentsInChildren<Hazard>().ToList();
-                        foreach(Ring ring in rings2) ring.RotateSpeed = 0f;
-                        foreach (Hazard e in enemies2) e.Speed = 0f; ;
-                        break;
-                    case SpeedMod.eSpeedModType.RING_REVERSE:
-                        player.CurChannel.MyRing.RotateSpeed = -player.CurChannel.MyRing.RotateSpeed;
-                        break;
-                    case SpeedMod.eSpeedModType.MEGA_RING_REVERSE: // moupdate - maybe rename this
-                        List<Ring> rings3 = _Dax.CurWheel.GetComponentsInChildren<Ring>().ToList();
-                        foreach (Ring ring in rings3) ring.RotateSpeed = -ring.RotateSpeed;
-                        break;*/
+                        break;                 
                 }
                 speedMod.SpawningNode.SpawnedBoardObject = null;
                 boardObjectColliders.Remove(speedMod.GetComponentInChildren<Collider>());
